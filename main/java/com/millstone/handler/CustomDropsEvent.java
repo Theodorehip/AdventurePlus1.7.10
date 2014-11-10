f887960ac8efbf38a2b307c05d03b2d349f84c24
Pandawan <pandawanfr@gmail.com>
2014-11-09 21:26:28 -0800

    Mini Fix
    mcmod.info still doesn’t work!

--- main/java/com/millstone/handler/CustomDropsEvent.java
+++ main/java/com/millstone/handler/CustomDropsEvent.java
@@ -2,11 +2,12 @@ package com.millstone.handler;
 
 import java.util.Random;
 
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+
+import com.millstone.registry.ItemRegistry;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class CustomDropsEvent {
@@ -17,15 +18,21 @@ public Random r = new Random();
 @SubscribeEvent
 public void onEntityDrop(LivingDropsEvent event)
 {
-	if(r.nextInt(100)<50){
-	if(event.entityLiving instanceof EntitySkeleton) {
-		event.drops.clear(); //Clears all the drops of the entity
-		event.entityLiving.dropItem(Items.bone, r.nextInt(2)); //drop the new items
+	
+	if(event.entityLiving instanceof EntitySkeleton){
+		
+		event.drops.clear(); //Clears all the drops of the entity   DON'T FORGET TO DO THIS! ALWAYS PUT IT, EVEN FOR RARE DROPS!
+		event.entityLiving.dropItem(Items.bone, r.nextInt(2));
+		event.entityLiving.dropItem(ItemRegistry.rottenArrow, r.nextInt(2));
+		
 	}
 	
-	//Put the next if statement here to add drops for other entities
 	
 	}
 
+	/*
+	 * Here is the "Rare Drops put them under this if statement"
+	 */
+	
+
 } 
-}
--- main/java/com/millstone/lib/References.java
+++ main/java/com/millstone/lib/References.java
@@ -3,7 +3,7 @@ package com.millstone.lib;
 public class References {
 	
 	public static final String MODID = "millstonemod";
-	public static final String VERSION = "0.0.4";
+	public static final String VERSION = "0.0.5";
 	public static final String Client = "com.millstone.lib.ClientProxy";
 	public static final String Common = "com.millstone.lib.CommonProxy";
 }
